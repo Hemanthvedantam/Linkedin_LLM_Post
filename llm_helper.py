c193555f862658048320539e5a4a331e51cf9847
Hemanth Hemanth <154527901+Hemanthvedantam@users.noreply.github.com>
2025-01-30 11:36:28 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,16 +1,17 @@
-from langchain_groq import ChatGroq
 import os
-from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 
-load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-text-preview")
+os.environ["GROQ_API_KEY"] = "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"
 
+
+api_key = os.getenv("GROQ_API_KEY")
+
+
+if not api_key:
+    raise ValueError("GROQ_API_KEY is not set. Please set it in your environment.")
+
+llm = ChatGroq(groq_api_key=api_key, model_name="llama-3.3-70b-versatile")
 
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
+    response = llm.invoke("Two most important ingredients in samosa are ")
     print(response.content)
-
-
-
-
-
